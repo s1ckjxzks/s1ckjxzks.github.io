dedfabdd2ea8b6e0b2b228409df1ea56d2fa4581
s1ckjxzks <jazalyn.heflin@jpschools.org>
2024-11-13 16:51:52 +0000

    adding new song and new hobbies

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,19 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circle;
+var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas);
+view.addChild(circle);
+circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+drawCircle(5);
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,7 +45,11 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+physikz.updatePosition(10, 10);
+  physikz.updatePosition();
+  physikz.updatePosition();
+  physikz.updatePosition();
+  physikz.updatePosition
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
